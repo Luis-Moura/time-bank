18ac0499ae036c048a27e9e433d38387d08fe7ec
Luis-Moura <luish.moura2024@gmail.com>
2025-10-19 20:39:14 -0300

    feat: adiciona middleware de erro e configurações de tratamento de erros na aplicação e a configuração do banco de dados no index.php :)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
+use MyApp\Handlers\HttpErrorHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -10,5 +12,34 @@ $app->setBasePath('/api/v1');
 
 //rotas
 require __DIR__ .'/../src/Routes/routes.php';
+
+//dotenv
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+//database
+require __DIR__ . '../src/Config/database.php';
+
+//configurações de erro
+$displayErrorDetails = true;
+$logErrors = true;
+$logErrorDetails = true;
+
+$errorMiddleware = new ErrorMiddleware(
+    $app->getCallableResolver(),
+    $app->getResponseFactory(),
+    $displayErrorDetails,
+    $logErrors,
+    $logErrorDetails
+);
+
+$errorHandler = new HttpErrorHandler(
+    $app->getCallableResolver(),
+    $app->getResponseFactory()
+);
+
+$errorMiddleware->setDefaultErrorHandler($errorHandler);
+
+$app->add($errorMiddleware);
 
 $app->run();
