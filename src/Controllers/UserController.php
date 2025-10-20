f650bd6e3b1dd6076b8fed8b5c2dc15dd592624f
Luis-Moura <luish.moura2024@gmail.com>
2025-10-20 17:02:21 -0300

    feat: adiciona método para adicionar habilidades ao usuário

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use App\Models\Skill;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use OpenApi\Attributes as OA;
@@ -37,6 +38,38 @@ class UserController
     $user = User::find($userId);
 
     $response->getBody()->write(json_encode($user));
+
+    return $response->withHeader('Content-Type', 'application/json');
+  }
+
+  public function addSkills(Request $request, Response $response)
+  {
+    $userId = $request->getAttribute('user_id');
+    $data = $request->getParsedBody();
+    $name = $data['name'];
+    $skillLevel = $data['skill_level'] ?? null;
+
+    if (!$name || !$skillLevel) {
+      $response->getBody()->write(json_encode(['error' => 'Missing fields']));
+
+      return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    }
+
+    $existingSkill = Skill::where('user_id', $userId)->where('name', $name)->first();
+
+    if ($existingSkill) {
+      $response->getBody()->write(json_encode(['error' => 'Skill already exists for user.']));
+
+      return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    }
+
+    $skill = Skill::create([
+      'name' => $name,
+      'skill_level' => $skillLevel,
+      'user_id' => $userId
+    ]);
+
+    $response->getBody()->write(json_encode($skill));
 
     return $response->withHeader('Content-Type', 'application/json');
   }
