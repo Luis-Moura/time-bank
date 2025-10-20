277462b82da59b5bd566751127712bc1958355ea
Luis-Moura <luish.moura2024@gmail.com>
2025-10-19 22:16:30 -0300

    feat: cria o controlador de transações com método para criar novas transações

---
+++ src/Controllers/TransactionController.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Transaction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class TransactionController
+{
+  public function create(Request $request, Response $response)
+  {
+    $userId = $request->getAttribute('user_id');
+    $data = $request->getParsedBody();
+
+    $transaction = Transaction::create([
+      'from_user_id' => $userId,
+      'to_user_id' => $data['to_user_id'],
+      'hours' => $data['hours'],
+      'description' => $data['description']
+    ]);
+
+    $response->getBody()->write(json_encode($transaction));
+    return $response->withHeader('Content-Type', 'application/json');
+  }
+}
