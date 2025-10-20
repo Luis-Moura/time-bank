1005ac93e5e70bc2c53617e3ed546902043f2030
Luis-Moura <luish.moura2024@gmail.com>
2025-10-19 21:12:14 -0300

    feat: adiciona rotas para registro e login no controlador Auth

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,9 +1,13 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Controllers\AuthController;
 
 $app->get('/', function (Request $request, Response $response, array $args) {
   $response->getBody()->write("API TimeBank");
 
   return $response;
 });
+
+$app->post('/register', [$authController, 'register']);
+$app->post('/login', [$authController, 'login']);
