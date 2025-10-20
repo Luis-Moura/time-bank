773177ea4365d3f376d1010d2e8fbae2edf0e9cd
Luis-Moura <luish.moura2024@gmail.com>
2025-10-19 22:13:25 -0300

    feat: recria a tabela de usuários no banco de dados

--- src/Config/database/migrations/01_20251019_create_users_table.php
+++ src/Config/database/migrations/01_20251019_create_users_table.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+Capsule::schema()->dropIfExists('users');
 Capsule::schema()->create('users', function ($table) {
     $table->id();
     $table->string('name');
