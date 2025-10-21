56d9b43322a0c351e8bc0a055e7d4318bd375179
Luis-Moura <luish.moura2024@gmail.com>
2025-10-20 22:01:23 -0300

    refactor: muda os metodos relacionados à skills para um controller separado

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 
 use App\Models\User;
-use App\Models\Skill;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use OpenApi\Attributes as OA;
@@ -38,48 +37,6 @@ class UserController
     $user = User::find($userId);
 
     $response->getBody()->write(json_encode($user));
-
-    return $response->withHeader('Content-Type', 'application/json');
-  }
-
-  public function addSkills(Request $request, Response $response)
-  {
-    $userId = $request->getAttribute('user_id');
-    $data = $request->getParsedBody();
-    $name = $data['name'];
-    $skillLevel = $data['skill_level'] ?? null;
-
-    if (!$name || !$skillLevel) {
-      $response->getBody()->write(json_encode(['error' => 'Missing fields']));
-
-      return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-    }
-
-    $existingSkill = Skill::where('user_id', $userId)->where('name', $name)->first();
-
-    if ($existingSkill) {
-      $response->getBody()->write(json_encode(['error' => 'Skill already exists for user.']));
-
-      return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-    }
-
-    $skill = Skill::create([
-      'name' => $name,
-      'skill_level' => $skillLevel,
-      'user_id' => $userId
-    ]);
-
-    $response->getBody()->write(json_encode($skill));
-
-    return $response->withHeader('Content-Type', 'application/json');
-  }
-
-  public function listSkills(Request $request, Response $response)
-  {
-    $userId = $request->getAttribute('user_id');
-    $skills = Skill::where('user_id', $userId)->get();
-
-    $response->getBody()->write(json_encode($skills));
 
     return $response->withHeader('Content-Type', 'application/json');
   }
--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -5,6 +5,7 @@ use App\Middlewares\AuthMiddleware;
 use App\Controllers\AuthController;
 use App\Controllers\TransactionController;
 use App\Controllers\UserController;
+use App\Controllers\SkillsController;
 
 $app->get('/', function (Request $request, Response $response, array $args) {
   $response->getBody()->write("API TimeBank");
@@ -22,6 +23,8 @@ $app->patch('/transactions/{id}/reject', [TransactionController::class, 'reject'
 $app->get('/transactions/incoming', [TransactionController::class, 'incoming'])->add(new AuthMiddleware());
 $app->get('/transactions/available-users', [TransactionController::class, 'getAvailableUsers'])->add(new AuthMiddleware());
 
+$app->post('/skills', [SkillsController::class, 'addSkills'])->add(new AuthMiddleware());
+$app->get('/skills', [SkillsController::class, 'listSkills'])->add(new AuthMiddleware());
+
 $app->get('/me', [UserController::class, 'me'])->add(new AuthMiddleware());
-$app->post('/users/skills', [UserController::class, 'addSkills'])->add(new AuthMiddleware());
-$app->get('/users/skills', [UserController::class, 'listSkills'])->add(new AuthMiddleware());
+
